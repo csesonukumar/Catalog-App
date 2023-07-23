4534c8492c52aa831eb3afb778bb0d2467f8eb30
sonu <sonushivkr@gmail.com>
2023-07-23 14:48:24 +0530

    day4

--- lib/Screens/loginScreen.dart
+++ lib/Screens/loginScreen.dart
@@ -6,13 +6,52 @@ class loginscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text(
-          "Login Screen",
-      style:TextStyle(
-        fontSize: 44,
-        color: Colors.red,
-      ),
-      ),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 40,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    labelText: "Enter Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    labelText: "Enter Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Day-4");
+                  },
+                  style: TextButton.styleFrom(),
+                  child: Text("Login"),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
